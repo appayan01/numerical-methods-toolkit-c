02140fa92a70f1261a75d2351dcd3d850e2425ec
Appayan Mondal <110631226+appayan01@users.noreply.github.com>
2026-08-28 11:01:38 +0530

    Add Newton-Raphson method interface

---
+++ include/newton.h
@@ -0,0 +1,10 @@
+#ifndef NEWTON_H
+#define NEWTON_H
+
+#include "result.h"
+
+typedef double (*Function)(double);
+
+Result newton_raphson(Function f, Function df, double x0, double tolerance);
+
+#endif
