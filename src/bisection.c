bac6eb5f0079f7995402bc1e856ce710a3535202
Appayan Mondal <110631226+appayan01@users.noreply.github.com>
2026-08-28 10:52:00 +0530

    Update Bisection with convergence results

--- src/bisection.c
+++ src/bisection.c
@@ -1,15 +1,20 @@
 #include "bisection.h"
 
-double bisection(Function f, double a, double b, double tolerance)
+Result bisection(Function f, double a, double b, double tolerance)
 {
+    Result result;
     double c;
+    int iteration = 0;
 
     while ((b - a) >= tolerance)
     {
         c = (a + b) / 2.0;
+        iteration++;
 
         if (f(c) == 0.0)
         {
+            a = c;
+            b = c;
             break;
         }
         else if (f(a) * f(c) < 0)
@@ -22,5 +27,9 @@ double bisection(Function f, double a, double b, double tolerance)
         }
     }
 
-    return (a + b) / 2.0;
+    result.root = (a + b) / 2.0;
+    result.iterations = iteration;
+    result.error = (b - a) / 2.0;
+
+    return result;
 }
