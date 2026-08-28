01ff03e98f0cc3c21e4ae645207e07bc2e01f0e3
Appayan Mondal <110631226+appayan01@users.noreply.github.com>
2026-08-28 10:41:31 +0530

    Connect main program to Bisection module

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include "bisection.h"
 
-int main(void) {
+double function(double x)
+{
+    return x * x * x - x - 2;
+}
+
+int main(void)
+{
+    double a = 1.0;
+    double b = 2.0;
+    double tolerance = 0.000001;
+    double root;
+
+    root = bisection(function, a, b, tolerance);
+
     printf("Numerical Methods Toolkit\n");
+    printf("-------------------------\n");
+    printf("Method: Bisection\n");
+    printf("Function: x^3 - x - 2\n");
+    printf("Interval: [%.1f, %.1f]\n", a, b);
+    printf("Tolerance: %.6f\n", tolerance);
+    printf("Approximate root: %.6f\n", root);
+
     return 0;
 }
