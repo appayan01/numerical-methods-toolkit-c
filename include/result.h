7ba738536a115e19e69d766e0d93f8feea49cc20
Appayan Mondal <110631226+appayan01@users.noreply.github.com>
2026-08-28 12:23:25 +0530

    Add convergence status to numerical results

--- include/result.h
+++ include/result.h
@@ -1,11 +1,21 @@
 #ifndef RESULT_H
 #define RESULT_H
 
+typedef enum
+{
+    SUCCESS,
+    MAX_ITERATIONS,
+    INVALID_INTERVAL,
+    ZERO_DERIVATIVE,
+    ZERO_DENOMINATOR
+} Status;
+
 typedef struct
 {
     double root;
     int iterations;
     double error;
+    Status status;
 } Result;
 
 #endif
